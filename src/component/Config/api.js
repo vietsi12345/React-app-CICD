6e0f963ce6e2072672b75fd99714fe729148c5de
vietsi12345 <nguyenvietsi12@gmail.com>
2024-06-20 20:05:00 +0700

    mm

--- src/component/Config/api.js
+++ src/component/Config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 // export const API_URL = "http://localhost:8088"
-export const API_URL = 'http://localhost/api'
+export const API_URL = 'http://localhost/si'
 
 export const api = axios.create({
     baseURL: API_URL,
